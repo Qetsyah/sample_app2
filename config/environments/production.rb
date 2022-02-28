e336613dbce0d112fb0038bbd4b43e8de47e8551
Patricia <patricia_paulo99@hotmail.com>
2022-02-28 16:05:38 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-ocean-92290.herokuapp.com'
+  host = 'https://quiet-ocean-92290.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
